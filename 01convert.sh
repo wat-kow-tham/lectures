be3a56fce69b1abc1bf6425026ca1b77e47b3af4
Anatoli Babenia <anatoli@rainforce.org>
2018-05-17 12:52:39 +0300

    Convert all files from .m4a to .mp4 with blank video channel

--- 01convert.sh
+++ 01convert.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 for filename in *.m4a; do
-  echo ${filename%.*}
+  REC=${filename%.*}
+  ffmpeg -hide_banner -i "${REC}.m4a" -c:a copy -filter_complex color=white -shortest -preset ultrafast -r 5 "${REC}.mp4"
 done
-
-#REC="20180412_002"; time ffmpeg -hide_banner -i "${REC}.m4a" -c:a copy -filter_complex color=white -shortest -preset ultrafast -r 5 "${REC}.mp4"
